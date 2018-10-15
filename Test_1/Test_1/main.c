5267096d2456184644f8fab50b9688229eda9ca1
Gachiman <aglikov@bk.ru>
2018-10-16 01:51:41 +0300

    Task4_V1.0

--- Test_1/Test_1/main.c
+++ Test_1/Test_1/main.c
@@ -1,23 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h>
 #include "mpi.h"
 
-#define N 10
+#define N 300
 
 int main(int argc, char* argv[]) {
+    srand(time(NULL));
     int ProcNum, ProcRank, RecvRank;
-    int vector[N] = { 60, 30, 70, 50, 10, 15, 14, 13, 12, 11 };
-    int min = 200;
-    int ProcMin = 200;
+    int vector[N];  // Объявляем и определяем одномерный пассив
+    for (int i = 0; i < N; i++)
+        vector[i] = rand();
+    int min = INT_MAX;
+    int ProcMin = INT_MAX;
     MPI_Status Status;
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
     MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
+
+    // Выполняется процесс 0
     if (ProcRank == 0) {
+        for (int j = 0; j < N; j++)
+            printf("%6d  ", vector[j]);
         for (int i = 0; i < N / ProcNum; i++) {
             if (vector[i] < min)
                 min = vector[i];
         }
-        printf("\n Minimum from process %d = %d", ProcRank, min);
+        printf("\n\n Minimum from process %d = %d", ProcRank, min);
         for (int i = 1; i < ProcNum; i++) {
             MPI_Recv(&ProcMin, 1, MPI_INT, i,
                 MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
@@ -27,6 +37,7 @@ int main(int argc, char* argv[]) {
         }
         printf("\n\n Total minimum = %d\n", min);
     }
+    // Выполняются остальные процессы
     else {
         if (ProcRank == ProcNum - 1) {
             for (int i = ProcRank * (N / ProcNum); i < N; i++)
