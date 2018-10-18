757681176fe9dff955c34f49f94c6e0d377e6f17
Gachiman <aglikov@bk.ru>
2018-10-18 06:05:14 +0300

    V2.0 new code

--- Test_1/Test_1/main.c
+++ Test_1/Test_1/main.c
@@ -4,25 +4,25 @@
 #include <limits.h>
 #include "mpi.h"
 
-#define N 300
+#define N 100
 
-int main(int argc, char* argv[]) {
+/*int main(int argc, char* argv[]) {
     srand(time(NULL));
-    int ProcNum, ProcRank, RecvRank;
+    int ProcNum, ProcRank;
     int vector[N];  // Объявляем и определяем одномерный пассив
-    for (int i = 0; i < N; i++)
-        vector[i] = rand();
-    int min = INT_MAX;
-    int ProcMin = INT_MAX;
+    int min = INT_MAX, ProcMin = INT_MAX;
     MPI_Status Status;
+
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
     MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
 
-    // Выполняется процесс 0
+    // Process 0
     if (ProcRank == 0) {
-        for (int j = 0; j < N; j++)
-            printf("%6d  ", vector[j]);
+        for (int i = 0; i < N; i++) {
+            vector[i] = rand();
+            printf("%6d  ", vector[i]);
+        }
         for (int i = 0; i < N / ProcNum; i++) {
             if (vector[i] < min)
                 min = vector[i];
@@ -37,7 +37,7 @@ int main(int argc, char* argv[]) {
         }
         printf("\n\n Total minimum = %d\n", min);
     }
-    // Выполняются остальные процессы
+    // Others process
     else {
         if (ProcRank == ProcNum - 1) {
             for (int i = ProcRank * (N / ProcNum); i < N; i++)
@@ -53,4 +53,52 @@ int main(int argc, char* argv[]) {
     }
     MPI_Finalize();
     return 0;
+}*/
+
+void DataInitialization(int *x);
+
+int main(int argc, char* argv[]) {
+    int vector[N], ProcMin = INT_MAX, TotalMin;
+    int ProcRank, ProcNum;
+    MPI_Status Status;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
+    MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
+    if (ProcRank == 0)
+        DataInitialization(vector);
+    MPI_Bcast(&vector, N, MPI_INT, 0, MPI_COMM_WORLD);
+    int part = N / ProcNum;
+    int i1 = part * ProcRank;
+    int i2 = (ProcRank == ProcNum - 1)? N : part * (ProcRank + 1);
+    for (; i1 < i2; i1++)
+        if (ProcMin > vector[i1])
+            ProcMin = vector[i1];
+
+    if (ProcRank == 0) {  // Process 0
+        TotalMin = ProcMin;
+        /*for (int i = 1; i < ProcNum; i++) {
+            MPI_Recv(&ProcMin, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &Status);
+            if (TotalMin > ProcMin)
+                TotalMin = ProcMin;
+        }*/
+    }
+    else  // Others process
+        MPI_Send(&ProcMin, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+
+    MPI_Reduce(&ProcMin, &TotalMin, 1, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD);
+
+    // Print result
+    if (ProcRank == 0)
+        printf("\nTotal minimum = %d", TotalMin);
+    MPI_Finalize();
+    return 0;
+}
+
+void DataInitialization(int *x) {
+    srand(time(NULL));
+    for (int i = 0; i < N; i++) {
+        x[i] = rand();
+        printf("%6d  ", x[i]);
+    }
 }
