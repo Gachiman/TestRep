57e08284a189c74526ee0625edfbceb05075e002
Gachiman <aglikov@bk.ru>
2018-10-20 17:58:03 +0300

    v 7 v kalic idti

--- Test_1/Test_1/main.c
+++ Test_1/Test_1/main.c
@@ -3,12 +3,10 @@
 #include <time.h>
 #include "mpi.h"
 
-#define N 75  // Size is set via console - DONE
-
 void Initialization(int *vector, int size);
 int FindMin(int *vector, int part);
 
-int main(int argc, char* argv[]) {
+/*int main(int argc, char* argv[]) {
     int ProcMin = INT_MAX, TotalMin, size;
     int ProcRank, ProcNum;
     double timeStart, timeEnd;
@@ -45,7 +43,7 @@ int main(int argc, char* argv[]) {
         free(dummyVector);
     }
 
-    if (ProcNum > 1)
+    if (ProcNum == 1)
         MPI_Reduce(&ProcMin, &TotalMin, 1, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD);
 
     // Print result
@@ -55,7 +53,7 @@ int main(int argc, char* argv[]) {
     }
     MPI_Finalize();
     return 0;
-}
+}*/
 
 void Initialization(int *x, int size) {
     srand(time(NULL));
@@ -75,4 +73,50 @@ int FindMin(int *vector, int part) {
         if (vector[i] < min)
             min = vector[i];
     return min;
+}
+
+int main(int argc, char* argv[]) {
+    int ProcNum, ProcRank;
+    int max = INT_MAX, i;
+    int localMax, size;
+    double startTime, endTime;
+    MPI_Status Status;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
+    MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
+
+    if (ProcRank == 0)
+        size = (argc != 1) ? atoi(argv[1]) : 75;
+    MPI_Bcast(&size, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    int step = size / ProcNum;
+
+    if (ProcRank == 0) {
+        int* vector = malloc(size * sizeof(int));
+        Initialization(vector, size);
+        startTime = MPI_Wtime();
+        for (i = 1; i < ProcNum - 1; i++)
+            MPI_Send(vector + step * i, step, MPI_INT, i, 0, MPI_COMM_WORLD);
+        if (ProcNum > 1)
+            MPI_Send(vector + step * i, size - step * (ProcNum - 1), MPI_INT, i, 0, MPI_COMM_WORLD);
+        max = FindMin(vector, step);
+        free(vector);
+    }
+    else {
+        int localSize = (ProcRank != (ProcNum - 1)) ? step : (size - step * (ProcNum - 1));
+        int* recvBuf = malloc(localSize * sizeof(int));
+        MPI_Recv(recvBuf, localSize, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
+        localMax = FindMin(recvBuf, localSize);
+        free(recvBuf);
+    }
+    if (ProcNum > 1)
+        MPI_Reduce(&localMax, &max, 1, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD);
+
+    if (ProcRank == 0) {
+        endTime = MPI_Wtime();
+        printf("\nMax element - %d\nTime spent - %.6f", max, endTime - startTime);
+    }
+    MPI_Finalize();
+    return 0;
 }
